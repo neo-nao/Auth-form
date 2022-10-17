b49ba901334990133917ef60adc09c56e6f67e2b
Lucy chan <99690752+Lucy-chann@users.noreply.github.com>
2022-10-17 11:31:17 +0100

    baseURL updated

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://login-signup-form-yuki.heroku";
+axios.defaults.baseURL = "https://login-signup-form-yuki.herokuapp.com";
 
 const http = {
   get: axios.get,
